962a0d49e2c55db5c1daebb239fc2476c84f612c
Lorenzo Zanotto <contact@lorenzozanotto.com>
2016-03-30 00:24:59 +0200

    Initial Commit

---
+++ Canvasly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Canvasly
+//
+//  Created by Lorenzo Zanotto on 30/03/2016.
+//  Copyright © 2016 Lorenzo Zanotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
